0ebe2a3f8d3676e017f4d21a33f97d2331b69567
ChoiHanSam <choigo020419@gmail.com>
2022-10-11 16:39:49 +0900

    Dash coding...
    대쉬 구현중....

--- Curse of Parrying/Python/Test.py
+++ Curse of Parrying/Python/Test.py
@@ -16,6 +16,7 @@ class Player:
         self.frame = 0
         self.dir = 'idle'
         self.exdir = 'right'
+        self.dash = 'off'
 
     def draw(self):
         if self.dir == 'right':
@@ -38,6 +39,8 @@ class Player:
             elif self.exdir == 'left':
                 self.image.clip_draw(2 * 68, 0, 68, 68, self.x, self.y)
 
+
+
     def update(self):
         self.frame = (self.frame+1)%8
         if self.dir == 'right':
@@ -53,15 +56,58 @@ class Player:
         if self.dir == 'right' or self.dir == 'left':
             self.exdir = self.dir
 
+class Player_Dash:
+    def __init__(self):
+        self.image = load_image('../Object/Character/Dash/Character_Player_Dash.png')
+        self.x, self.y = 0, 0
+        self.frame = 0
+        self.dir = ' '
+        self.exdir = ' '
 
-def handle_events(Player):
+
+    def draw(self):
+        if self.dir == 'right':
+            self.image.clip_draw(self.frame*68, 68, 68, 68, self.x, self.y)
+        elif self.dir == 'left':
+            self.image.clip_draw(self.frame * 68, 0, 68, 68, self.x, self.y)
+        elif self.dir == 'up':
+            if self.exdir == 'right':
+                self.image.clip_draw(self.frame*68, 68, 68, 68, self.x, self.y)
+            elif self.exdir == 'left':
+                self.image.clip_draw(self.frame*68, 0, 68, 68, self.x, self.y)
+        elif self.dir == 'down':
+            if self.exdir == 'right':
+                self.image.clip_draw(self.frame*68, 68, 68, 68, self.x, self.y)
+            elif self.exdir == 'left':
+                self.image.clip_draw(self.frame*68, 0, 68, 68, self.x, self.y)
+        elif self.dir == 'idle':
+            if self.exdir == 'right':
+                self.image.clip_draw(2 * 68, 68, 68, 68, self.x, self.y)
+            elif self.exdir == 'left':
+                self.image.clip_draw(2 * 68, 0, 68, 68, self.x, self.y)
+
+
+    def set_dir(self, Player):
+        self.dir = Player.dir
+        self.exdir = Player.exdir
+
+    def update(self):
+        if self.dir == 'right':
+            self.x += 10
+        elif self.dir == 'left':
+            self.x -= 10
+        elif self.dir == 'up':
+            self.y += 10
+        elif self.dir == 'down':
+            self.y -= 10
+
+def handle_events(Player, Player_Dash):
     global running
     events = get_events()
-
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_KEYUP:
+        elif event.type == SDL_KEYUP and (event.key == SDLK_RIGHT or event.key == SDLK_LEFT or event.key == SDLK_UP or event.key == SDLK_DOWN):
             Player.dir = 'idle'
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
@@ -73,6 +119,8 @@ def handle_events(Player):
             Player.dir = 'up'
         elif event.type == SDL_KEYDOWN and event.key == SDLK_DOWN:
             Player.dir = 'down'
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_z:
+            Player.dash = 'on'
 
 open_canvas()
 
@@ -80,21 +128,43 @@ running = True
 
 Player = Player()
 Background = Background()
+Player_Dash = Player_Dash()
 
 while running:
     clear_canvas()
     Background.draw()
-    print(Player.exdir)
     Player.draw()
     update_canvas()
-    handle_events(Player)
+    handle_events(Player, Player_Dash)
+    if Player.dash == 'on':
+        Player_Dash.set_dir(Player)
+        for a in range(8):
+            print('a')
+            clear_canvas()
+            Background.draw()
+            Player_Dash.draw()
+            update_canvas()
+            Player_Dash.update()
+            delay(0.01)
+        Player.dash = 'off'
     while Player.dir != 'idle':
         clear_canvas()
         Background.draw()
         Player.draw()
         update_canvas()
         Player.set_exdir()
-        handle_events(Player)
+        handle_events(Player, Player_Dash)
+        if Player.dash == 'on':
+            Player_Dash.set_dir(Player)
+            for a in range(8):
+                print('a')
+                clear_canvas()
+                Background.draw()
+                Player_Dash.draw()
+                update_canvas()
+                Player_Dash.update()
+                delay(0.01)
+            Player.dash = 'off'
         Player.update()
         delay(0.05)
     delay(0.05)
