e0582a93313c1eb6957d6ef8d61f3554c82b94c4
ChoiHanSam <choigo020419@gmail.com>
2022-10-13 18:06:29 +0900

    TestMap

--- Curse of Parrying/Python/Test.py
+++ Curse of Parrying/Python/Test.py
@@ -1,20 +1,64 @@
 from pico2d import *
 
 
+# 테스트용 몬스터 클래스
+class Test_Monster:
+    def __init__(self):
+        self.image = load_image('../Object/Enemy/Stage1/Pigeon/Enemy_Pigeon_Attack.png')
+        self.x, self.y = 100, 200
+        self.frame = 0
+        self.summon = 'off'
+
+    def draw(self):
+        if self.summon == 'on':
+            self.image.clip_draw(self.frame*68, 0, 68, 68, self.x, self.y)
+
+    def update(self):
+        if self.summon == 'on':
+            self.frame = (self.frame+1)%9
+
+
+# 테스트용 몬스터의 공격 이펙트 클래스
+class Test_Monster_Attack_Effect:
+    def __init__(self):
+        self.image = load_image('../Effect/Monster/Monster_Attack/Pigeon/Pigeon_Attack.png')
+        self.x, self.y = 100, 200
+        self.frame = 0
+        self.judge = 'off'
+
+    def draw(self):
+        if self.judge == 'on':
+            if self.x < 700:
+                self.image.clip_draw(self.frame*68, 0, 68, 68, self.x, self.y)
+
+    def update(self):
+        if self.judge == 'on':
+            self.frame = (self.frame+1)%7
+            self.x += 10
+        if self.x > 700:
+            self.judge = 'off'
+
+
+# 상호작용 클래스
 class Interact:
     def __init__(self):
         self.image = load_image('../Effect/ETC/interact.png')
         self.x, self.y = 0, 0
+        self.judge = 'off'
 
     def draw(self, Object):
         bndry = 50
         if (Object.x + bndry > self.x > Object.x - bndry) and (Object.y + bndry > self.y > Object.y - bndry):
+            self.judge = 'on'
             self.image.draw(self.x, self.y)
+        else:
+            self.judge = 'off'
 
     def set_xy(self, Player):
         self.x, self.y = Player.x, Player.y
 
 
+# 몬스터 박스 클래스
 class MonsterBox:
     def __init__(self):
         self.image = load_image('../Object/ETC/MonsterBox.png')
@@ -164,7 +208,7 @@ class Player_Parrying:
 
 
 # 이벤트 받기 함수
-def handle_events(Player):
+def handle_events(Player, Monster, Interact):
     global running
     events = get_events()
     for event in events:
@@ -187,6 +231,13 @@ def handle_events(Player):
             Player.dash = 'on'
         elif event.type == SDL_KEYDOWN and event.key == SDLK_x:
             Player.parrying = 'on'
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_f:
+            if Interact.judge == 'on':
+                if Monster.summon == 'off':
+                    Monster.summon = 'on'
+                elif Monster.summon == 'on':
+                    Monster.summon = 'off'
+                    Monster.frame = 0
 
 
 # 기본 세팅
@@ -198,18 +249,44 @@ Player_Dash = Player_Dash()
 Player_Parrying = Player_Parrying()
 MonsterBox = MonsterBox()
 Interact = Interact()
+Test_Monster = Test_Monster()
+Effect = [Test_Monster_Attack_Effect() for i in range(1)]
+num = 0
+num2 = 1
 
 # 게임 내부
 while running:
     # 멈춤상태
     clear_canvas()
     Background.draw()
+    # 캐릭터 그리기
     Player.draw()
+    # 상호작용 그리기
     Interact.set_xy(Player)
     Interact.draw(MonsterBox)
+    # 몬스터 박스 그리기
     MonsterBox.draw()
+    # 테스트 몬스터 그리기
+    Test_Monster.draw()
+    Test_Monster.update()
+    # 테스트 몬스터의 공격 이펙트 그리기
+    if Test_Monster.summon == 'on':
+        num2 = (num2+1)%2
+        for effect in Effect:
+            effect.draw()
+            effect.update()
+            if len(Effect) == 10:
+                Effect.pop(0)
+        if num2 == 0:
+            if Test_Monster.frame == 3:
+                Effect.append(Test_Monster_Attack_Effect())
+                Effect[num].judge = 'on'
+                if num < 9:
+                    num += 1
+    # 캔버스 업데이트
     update_canvas()
-    handle_events(Player)
+    # 입력 받기
+    handle_events(Player, Test_Monster, Interact)
     # 대쉬
     if Player.dash == 'on':
         Player_Dash.set_dir(Player)
@@ -218,6 +295,21 @@ while running:
             Background.draw()
             Player_Dash.draw()
             MonsterBox.draw()
+            Test_Monster.draw()
+            Test_Monster.update()
+            if Test_Monster.summon == 'on':
+                num2 = (num2 + 1) % 2
+                for effect in Effect:
+                    effect.draw()
+                    effect.update()
+                    if len(Effect) == 10:
+                        Effect.pop(0)
+                if num2 == 0:
+                    if Test_Monster.frame == 3:
+                        Effect.append(Test_Monster_Attack_Effect())
+                        Effect[num].judge = 'on'
+                        if num < 9:
+                            num += 1
             update_canvas()
             Player_Dash.update()
             delay(0.01)
@@ -230,9 +322,24 @@ while running:
             Background.draw()
             Player_Parrying.draw()
             MonsterBox.draw()
+            Test_Monster.draw()
+            Test_Monster.update()
+            if Test_Monster.summon == 'on':
+                num2 = (num2 + 1) % 2
+                for effect in Effect:
+                    effect.draw()
+                    effect.update()
+                    if len(Effect) == 10:
+                        Effect.pop(0)
+                if num2 == 0:
+                    if Test_Monster.frame == 3:
+                        Effect.append(Test_Monster_Attack_Effect())
+                        Effect[num].judge = 'on'
+                        if num < 9:
+                            num += 1
             update_canvas()
             Player_Parrying.update()
-            delay(0.04)
+            delay(0.05)
         Player.parrying = 'off'
     # 움직임 상태
     while Player.dir != 'idle':
@@ -242,9 +349,24 @@ while running:
         Interact.set_xy(Player)
         Interact.draw(MonsterBox)
         MonsterBox.draw()
+        Test_Monster.draw()
+        Test_Monster.update()
+        if Test_Monster.summon == 'on':
+            num2 = (num2 + 1) % 2
+            for effect in Effect:
+                effect.draw()
+                effect.update()
+                if len(Effect) == 10:
+                    Effect.pop(0)
+            if num2 == 0:
+                if Test_Monster.frame == 3:
+                    Effect.append(Test_Monster_Attack_Effect())
+                    Effect[num].judge = 'on'
+                    if num < 9:
+                        num += 1
         update_canvas()
         Player.set_exdir()
-        handle_events(Player)
+        handle_events(Player, Test_Monster, Interact)
         # 대쉬
         if Player.dash == 'on':
             Player_Dash.set_dir(Player)
@@ -253,6 +375,21 @@ while running:
                 Background.draw()
                 Player_Dash.draw()
                 MonsterBox.draw()
+                Test_Monster.draw()
+                Test_Monster.update()
+                if Test_Monster.summon == 'on':
+                    num2 = (num2 + 1) % 2
+                    for effect in Effect:
+                        effect.draw()
+                        effect.update()
+                        if len(Effect) == 10:
+                            Effect.pop(0)
+                    if num2 == 0:
+                        if Test_Monster.frame == 3:
+                            Effect.append(Test_Monster_Attack_Effect())
+                            Effect[num].judge = 'on'
+                            if num < 9:
+                                num += 1
                 update_canvas()
                 Player_Dash.update()
                 delay(0.01)
@@ -265,9 +402,24 @@ while running:
                 Background.draw()
                 Player_Parrying.draw()
                 MonsterBox.draw()
+                Test_Monster.draw()
+                Test_Monster.update()
+                if Test_Monster.summon == 'on':
+                    num2 = (num2 + 1) % 2
+                    for effect in Effect:
+                        effect.draw()
+                        effect.update()
+                        if len(Effect) == 10:
+                            Effect.pop(0)
+                    if num2 == 0:
+                        if Test_Monster.frame == 3:
+                            Effect.append(Test_Monster_Attack_Effect())
+                            Effect[num].judge = 'on'
+                            if num < 9:
+                                num += 1
                 update_canvas()
                 Player_Parrying.update()
-                delay(0.04)
+                delay(0.05)
             Player.parrying = 'off'
         Player.update()
         delay(0.05)
